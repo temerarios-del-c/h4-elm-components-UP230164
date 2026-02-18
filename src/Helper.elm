e8fe7bbe1e4cce29884c7b786ba194264e14a1fd
UP230164 <up230164@alumnos.upa.edu.mx>
2026-02-18 02:39:08 -0600

    Primer commit

--- src/Helper.elm
+++ src/Helper.elm
@@ -4,8 +4,33 @@ import Html
 import Html.Attributes
 
 
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html.Html msg
+headers str =
+    Html.div []
+        [ Html.h1 [] [ Html.text str ]
+        , Html.h2 [] [ Html.text str ]
+        , Html.h3 [] [ Html.text str ]
+        , Html.h4 [] [ Html.text str ]
+        , Html.h5 [] [ Html.text str ]
+        , Html.h6 [] [ Html.text str ]
+        ]
+
+
+hyperlink : String -> String -> Html.Html msg
+hyperlink link text =
+    Html.a [ Html.Attributes.href link ] [ Html.text text ]
